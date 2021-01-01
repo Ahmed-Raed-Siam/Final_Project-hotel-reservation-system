42ea28c4e5177b21e7035db44657739ab28f52c0
Ahmed-Raed-Siam <68457689+Ahmed-Raed-Siam@users.noreply.github.com>
2021-01-01 22:20:55 +0200

    Add Api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('test', function () {
+    return "<h1>My name is Ahmed Raed Siam - <small>120170736</small></h1>";
+});
